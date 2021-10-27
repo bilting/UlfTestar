312f98a28ebbd10658a8623fc776e114bbbc6e2a
Ulf Bilting <ulf@bilting.se>
2021-10-27 17:07:13 +0200

    first version

--- src/se/bilting/Main.java
+++ src/se/bilting/Main.java
@@ -3,6 +3,6 @@ package se.bilting;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Hello, world!");
     }
 }
